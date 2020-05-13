38458b54157af7ab51789421eb5c6983139e71a4
Jiwon <jiwon1.han@gmail.com>
2020-05-12 17:01:43 -0700

    complete connecting with database

---
+++ GetHeroes/index.js
@@ -0,0 +1,34 @@
+var MongoClient = require('mongodb').MongoClient;
+
+// using funcpack now
+
+module.exports = function(context, req) {
+  MongoClient.connect(process.env.CosmosDBConnectionString, (err, client) => {
+    let send = response(client, context);
+
+    if (err) send(500, err.message);
+
+    let db = client.db('admin');
+
+    db
+      .collection('heros')
+      .find({})
+      .toArray((err, result) => {
+        if (err) send(500, err.message);
+
+        send(200, JSON.parse(JSON.stringify(result)));
+      });
+  });
+};
+
+function response(client, context) {
+  return function(status, body) {
+    context.res = {
+      status: status,
+      body: body
+    };
+
+    client.close();
+    context.done();
+  };
+}
